9f5e38955cb0d9e6d9049d1687ffe6234fbd208b
Anurag Pant <pantanurag10@outlook.com>
2025-02-23 14:59:44 +0530

    Create NumberPattern1.java

---
+++ NumberPattern1.java
@@ -0,0 +1,13 @@
+public class JavaProject {
+	public static void main(String[] args) {
+//Outer loop for printing rows
+		for (int i = 1; i <= 5; i++) {
+//		Inner loop for printing row
+			for (int j = 1; j <= 5; j++) {
+				System.out.println(i);
+			}
+
+			System.out.println();
+		}
+	}
+}
